10c4c712c8a54109a8a7bd327c6beb554afafc11
trieunhuyenquoc <trieunguyenquoc020604@gmai.com>
2023-01-18 09:06:42 +0700

    https://github.com/TrieuNguyenQuoc/classlinkeslist.git

---
+++ src/TestMyLinkkedlist.java
@@ -0,0 +1,13 @@
+public class TestMyLinkkedlist {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        MyLinkedlist ll = new MyLinkedlist(10);
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.addFirst(13);
+
+        ll.add(4,9);
+        ll.add(4,9);
+        ll.printList();
+    }
+}
